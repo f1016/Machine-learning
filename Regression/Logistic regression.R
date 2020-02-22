8ed68658f7c2907093df56761fb217dc38d565f5
f1016 <47860894+f1016@users.noreply.github.com>
2020-02-23 03:27:16 +0800

    Add files via upload

---
+++ Regression/Logistic regression.R
@@ -0,0 +1,45 @@
+table <- read.table("LogitRegData.txt", header = TRUE, sep =" ", dec = ".")
+
+
+# x = table[i,1,1]
+# y = table[i,2,1]
+
+Mat <- matrix(c(1.5,1.6),nrow = 2, ncol = 1, byrow = TRUE) # by approximation 
+TBT_M <- matrix(0, nrow = 2, ncol = 2, byrow = TRUE)
+TBO_M <- matrix(0, nrow = 2, ncol = 1, byrow =TRUE)
+i <- 1
+j <- 1
+
+while(j != 50){
+  
+  while(i != 51){
+    
+    TBT_M[1, 1] <- TBT_M[1, 1] - (exp( Mat[1,1]+Mat[2,1]*table[i,1,1]) / (exp(Mat[1,1] + Mat[2,1]*table[i,1,1]) + 1)^2)
+    TBT_M[1, 2] <- TBT_M[1, 2] - table[i,1,1]*(exp(Mat[1,1]+Mat[2,1]*table[i,1,1])) / (exp(Mat[1,1] + Mat[2,1]*table[i,1,1])+1)^2
+    TBT_M[2, 1] <- TBT_M[2, 1] - table[i,1,1]*(exp(Mat[1,1]+Mat[2,1]*table[i,1,1])) / (exp(Mat[1,1] + Mat[2,1]*table[i,1,1])+1)^2
+    TBT_M[2, 2] <- TBT_M[2, 2] - (table[i,1,1]^2)*(exp(Mat[1,1]+Mat[2,1]*table[i,1,1])) / (exp(Mat[1,1] + Mat[2,1]*table[i,1,1])+1)^2
+    
+    TBO_M[1, 1] <- TBO_M[1 ,1] + table[i,2,1] - (exp( Mat[1,1]+Mat[2,1]*table[i,1,1]) / (exp(Mat[1,1] + Mat[2,1]*table[i,1,1])+1))
+    TBO_M[2, 1] <- TBO_M[2 ,1] + table[i,2,1]*table[i,1,1] - table[i,1,1]*(exp( Mat[1,1]+Mat[2,1]*table[i,1,1]) / (exp(Mat[1,1] + Mat[2,1]*table[i,1,1])+1))
+    
+    i <- i + 1 
+  }
+  
+  Mat = Mat - solve(TBT_M)%*%TBO_M
+  TBT_M <- matrix(0, nrow = 2, ncol = 2, byrow = TRUE)
+  TBO_M <- matrix(0, nrow = 2, ncol = 1, byrow = TRUE)
+  i <- 1
+  j <- j + 1
+  
+}
+print(Mat)
+
+
+plot( -4:4, type="n")
+curve(exp( Mat[1,1] + Mat[2,1]*x)/(exp(Mat[1,1] + Mat[2,1]*x)+1),-4,4)
+i<- 1
+while(i != 51){
+  points(table[i,1,1],table[i,2,1])
+  i <- i+1
+}
+
---
+++ Regression/Poisson regression.R
@@ -0,0 +1,52 @@
+table <- read.table("PoisRegData.txt", header = TRUE, sep =" ", dec = ".")
+
+# x = table[i,1,1]
+# y = table[i,2,1]
+
+Mat <- matrix(c(3,2,2),nrow = 3, ncol = 1, byrow = TRUE) # by approximation 
+TBT_M <- matrix(0, nrow = 3, ncol = 3, byrow = TRUE)
+TBO_M <- matrix(0, nrow = 3, ncol = 1, byrow =TRUE)
+i <- 1
+j <- 1
+
+
+while(j != 50){
+  
+  Mat_old <- Mat
+  
+  while(i != 51){
+    
+    TBT_M[1, 1] <- TBT_M[1, 1] - exp(Mat[1,1]+ Mat[2,1]*table[i,1,1] + Mat[3,1]*(table[i,1,1])^2)
+    TBT_M[1, 2] <- TBT_M[1, 2] - table[i,1,1]*exp(Mat[1,1]+ Mat[2,1]*table[i,1,1] + Mat[3,1]*(table[i,1,1])^2)
+    TBT_M[1, 3] <- TBT_M[1, 3] - (table[i,1,1]^2)*exp(Mat[1,1]+ Mat[2,1]*table[i,1,1] + Mat[3,1]*(table[i,1,1])^2)
+    
+    TBT_M[2, 1] <- TBT_M[2, 1] - table[i,1,1]*exp(Mat[1,1]+ Mat[2,1]*table[i,1,1] + Mat[3,1]*(table[i,1,1])^2)
+    TBT_M[2, 2] <- TBT_M[2, 2] - (table[i,1,1]^2)*exp(Mat[1,1]+ Mat[2,1]*table[i,1,1] + Mat[3,1]*(table[i,1,1])^2)
+    TBT_M[2, 3] <- TBT_M[2, 3] - (table[i,1,1]^3)*exp(Mat[1,1]+ Mat[2,1]*table[i,1,1] + Mat[3,1]*(table[i,1,1])^2)
+    
+    TBT_M[3, 1] <- TBT_M[3, 1] - (table[i,1,1]^2)*exp(Mat[1,1]+ Mat[2,1]*table[i,1,1] + Mat[3,1]*(table[i,1,1])^2)
+    TBT_M[3, 2] <- TBT_M[3, 2] - (table[i,1,1]^3)*exp(Mat[1,1]+ Mat[2,1]*table[i,1,1] + Mat[3,1]*(table[i,1,1])^2)
+    TBT_M[3, 3] <- TBT_M[3, 3] - (table[i,1,1]^4)*exp(Mat[1,1]+ Mat[2,1]*table[i,1,1] + Mat[3,1]*(table[i,1,1])^2)
+    
+    TBO_M[1, 1] <- TBO_M[1, 1] + table[i,2,1] - exp(Mat[1,1]+ Mat[2,1]*table[i,1,1] + Mat[3,1]*(table[i,1,1])^2)
+    TBO_M[2, 1] <- TBO_M[2, 1] + table[i,1,1]*table[i,2,1] - table[i,1,1]*exp(Mat[1,1]+ Mat[2,1]*table[i,1,1] + Mat[3,1]*(table[i,1,1])^2)
+    TBO_M[3, 1] <- TBO_M[3, 1] + (table[i,1,1]^2)*table[i,2,1] - (table[i,1,1]^2)*exp(Mat[1,1]+ Mat[2,1]*table[i,1,1] + Mat[3,1]*(table[i,1,1])^2)
+    i <- i + 1 
+  }
+  
+  Mat = Mat - solve(TBT_M)%*%TBO_M
+  TBT_M <- matrix(0, nrow = 3, ncol = 3, byrow = TRUE)
+  TBO_M <- matrix(0, nrow = 3, ncol = 1, byrow = TRUE)
+  i <- 1
+  j <- j + 1
+  
+}
+print(Mat)
+plot( -1:1, type="n")
+curve(exp(Mat[1,1]+ Mat[2,1]*x + Mat[3,1]*(x)^2),-1,1)
+i<- 1
+while(i != 51){
+  points(table[i,1,1],table[i,2,1])
+  i <- i+1
+}
+
